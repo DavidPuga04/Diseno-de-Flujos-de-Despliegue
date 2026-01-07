cd15bdd123f2351ee9a2ec24580cb3fa489d41a6
David Puga <david.pugafudla.edu.ec>
2026-01-07 14:43:40 -0500

    test cargados y deber terminado

---
+++ src/test/java/com/example/ContextIT.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = MyappApplication.class)
+class ContextIT {
+
+    @Test
+    void contextLoads() {
+    }
+}
